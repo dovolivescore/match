ca5a6adfe1750a3eb4c9a682e066f436b272a9d9
dovolivescore <mrjoene@gmail.com>
2025-06-27 18:23:44 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "DOVO Livescore",
+  description: "Live wedstrijdverslag voor DOVO",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="nl">
-      <head />
       <body>{children}</body>
     </html>
   );
